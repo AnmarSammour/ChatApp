5bb9084bc73d5a30472a83907b1606a19831eb1b
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-14 01:34:56 +0300

    Rebuild the RegisterView

--- lib/view/register/register_view.dart
+++ lib/view/register/register_view.dart
@@ -1,10 +1,102 @@
+import 'package:chat_app/view/widgets/custom_button.dart';
+import 'package:chat_app/view/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
-class Register extends StatelessWidget {
-  const Register({super.key});
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
 
+class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xff396ca4),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 75,
+            ),
+            Image.asset(
+              'assets/images/chat_icon.png',
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Chat App',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 75,
+            ),
+            Row(
+              children: [
+                Text(
+                  'Register',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomFormTextField(
+              hintText: 'Email',
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CustomFormTextField(
+              hintText: 'Password',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomButon(
+              onTap: () {
+
+              },
+              text: 'Register',
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'already have an account?',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    '  LogIn',
+                    style: TextStyle(
+                      color: Color(0xffbfd3e9),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
