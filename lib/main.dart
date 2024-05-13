7ac7b9bd2d2aace34272312efb746a2ddebd2935
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-13 21:05:34 +0300

    Edit main code

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
+import 'package:chat_app/view/signin/signin.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Chat());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Chat extends StatelessWidget {
+  const Chat({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: SingInView()
     );
   }
 }
