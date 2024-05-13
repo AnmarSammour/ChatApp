d91e2b25204f07d9a0432085b712e7305c543a52
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-13 23:34:38 +0300

    Add and rebuild the CustomFormTextField page

---
+++ lib/view/signin/widgets/custom_text_field.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomFormTextField extends StatelessWidget {
+  String? hintText;
+
+  CustomFormTextField({
+    Key? key,
+    this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(
+          color: Colors.white,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
