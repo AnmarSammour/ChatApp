70d2cf6f0e61dc4fb6bb8e764e938e239a6c77db
AnmarSammour <anmarsammour2000@gmail.com>
2024-05-14 19:09:38 +0300

    Refactor code in RegisterView

--- lib/view/register/register_view.dart
+++ lib/view/register/register_view.dart
@@ -2,7 +2,10 @@ import 'package:chat_app/constants.dart';
 import 'package:chat_app/view/login/login.dart';
 import 'package:chat_app/view/widgets/custom_button.dart';
 import 'package:chat_app/view/widgets/custom_text_field.dart';
+import 'package:chat_app/view/widgets/show_snack_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -14,94 +17,137 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
+  String? email, password;
+
+  bool isLoading = false;
+
+  GlobalKey<FormState> formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: primaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: ListView(
-          children: [
-            SizedBox(
-              height: 75,
-            ),
-            Image.asset(
-              logo,
-              height: 100,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: primaryColor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Form(
+            key: formKey,
+            child: ListView(
               children: [
-                Text(
-                  'Chat App',
-                  style: TextStyle(
-                    fontSize: 32,
-                    color: Colors.white,
-                  ),
+                SizedBox(
+                  height: 75,
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 75,
-            ),
-            Row(
-              children: [
-                Text(
-                  'Register',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                  ),
+                Image.asset(
+                  logo,
+                  height: 100,
                 ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomFormTextField(
-              hintText: 'Email',
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CustomFormTextField(
-              hintText: 'Password',
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            CustomButon(
-              onTap: () {},
-              text: 'Register',
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'already have an account?',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, LogInView.id);
-                  },
-                  child: Text(
-                    '  LogIn',
-                    style: TextStyle(
-                      color: Color(0xffbfd3e9),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Chat App',
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
+                  ],
+                ),
+                SizedBox(
+                  height: 75,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomFormTextField(
+                  onChanged: (data) {
+                    email = data;
+                  },
+                  hintText: 'Email',
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomFormTextField(
+                  onChanged: (data) {
+                    password = data;
+                  },
+                  hintText: 'Password',
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                CustomButon(
+                  onTap: () async {
+                    if (formKey.currentState!.validate()) {
+                      isLoading = true;
+                      setState(() {});
+                      try {
+                        await registerUser();
+                      } on FirebaseAuthException catch (ex) {
+                        if (ex.code == 'weak-password') {
+                          showSnackBar(context, 'weak password');
+                        } else if (ex.code == 'email-already-in-use') {
+                          showSnackBar(context, 'email already exists');
+                        }
+                      } catch (ex) {
+                        showSnackBar(context, 'there was an error');
+                      }
+
+                      isLoading = false;
+                      setState(() {});
+                    } else {}
+                  },
+                  text: 'Register',
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'already have an account?',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, LogInView.id);
+                      },
+                      child: Text(
+                        '  LogIn',
+                        style: TextStyle(
+                          color: Color(0xffbfd3e9),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<UserCredential> registerUser() async {
+    UserCredential user = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email!, password: password!);
+    return user;
   }
 }
